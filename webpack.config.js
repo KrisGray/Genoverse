46d7929700aa8e914e798294b9cdfff97f1cf662
KrisGray <kris@ebi.ac.uk>
2018-01-12 11:55:08 +0000

    genoverse now builds into dist

--- webpack.config.js
+++ webpack.config.js
@@ -2,5 +2,5 @@ var webpack = require('webpack');
 
 module.exports = {
   entry   : __dirname + '/index.js',
-  output  : { filename: 'js/genoverse.min.js' }
+  output  : { filename: 'dist/js/genoverse.js' }
 };
